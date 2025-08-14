79234d75a8eaa2174d7cf0163a88c54e383f1340
Rocky-Coding <553666373@qq.com>
2025-08-14 14:31:16 +0800

    Site updated: YYYY-08-2025年8月14日 14:31:15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1755079486557";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1755153069135";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
